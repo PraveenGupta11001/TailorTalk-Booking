55db4ba80e037a9047e0413995888e0b196eeca7
Praveen Gupta <105053871+PraveenGupta11001@users.noreply.github.com>
2025-06-28 12:24:27 +0000

    testing environ

--- app/calendar_api.py
+++ app/calendar_api.py
@@ -3,6 +3,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import datetime
 import os.path
+import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from pytz import timezone
@@ -19,7 +20,8 @@ def get_calendar_service():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(os.environ.get("GOOGLE_CREDENTIALS"), SCOPES)
+            print(os.environ.get("GOOGLE_CREDENTIALS"))
             creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
