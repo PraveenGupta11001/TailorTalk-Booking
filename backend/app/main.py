090919a293a4344ae0dca9466c381e671629803d
PraveenGupta11001 <praweengupta11001@gmail.com>
2025-06-29 00:54:56 +0530

    adding backend server render

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://192.168.187.96:8501", '*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
